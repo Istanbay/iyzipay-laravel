f157f942ebc901ae6a07295ca641fd0b741cc4fe
Can <can@canerdogan.net>
2017-10-17 12:05:01 +0300

    Add softdelete traits for models

--- src/Models/CreditCard.php
+++ src/Models/CreditCard.php
@@ -5,9 +5,11 @@ namespace Iyzico\IyzipayLaravel\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CreditCard extends Model
 {
+	use SoftDeletes;
 
     protected $fillable = [
         'alias', 'number', 'token', 'bank'

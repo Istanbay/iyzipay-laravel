5264a8d8d66ac72110f229bb59f64b05adbd66da
Can <can@canerdogan.net>
2017-10-29 17:05:00 +0300

    Route fix

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::group( ['middleware' => 'web'], function() {
 
-	Route::post( '/iyzipay/threeds/callback', 'Iyzico\IyzipayLaravel\Controller\CallbackController@threedsCallback' )
+	Route::any( '/iyzipay/threeds/callback', 'Iyzico\IyzipayLaravel\Controller\CallbackController@threedsCallback' )
 	     ->name( 'threeds.callback' );
-	Route::post( '/iyzipay/bkm/callback', 'Iyzico\IyzipayLaravel\Controller\CallbackController@bkmCallback' )
+	Route::any( '/iyzipay/bkm/callback', 'Iyzico\IyzipayLaravel\Controller\CallbackController@bkmCallback' )
 	     ->name( 'bkm.callback' );
 });
